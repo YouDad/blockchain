aaca4f109e961579bad6b528cfd98f7a80b9b6c6
manjaro-pc <1632083718@qq.com>
2020-02-20 16:08:15 +0000

    RM: text

--- main.go
+++ main.go
@@ -4,21 +4,12 @@ import (
 	"log"
 
 	"github.com/YouDad/blockchain/app/coin"
-	"github.com/YouDad/blockchain/app/text"
 )
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.Lmicroseconds | log.Ltime)
 	log.SetPrefix("[info]: ")
 
-	app := 1
-	switch app {
-	case 0:
-		text.Init()
-		text.Main()
-	case 1:
-		coin.Init()
-		coin.Main()
-	default:
-	}
+	coin.Init()
+	coin.Main()
 }
