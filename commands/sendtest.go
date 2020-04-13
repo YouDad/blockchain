60738820bf70f5b4ca12b628ce05f0e8f2efca07
manjaro-pc <1632083718@qq.com>
2020-04-13 19:59:15 +0800

    UPDATE: SendTest

--- commands/sendtest.go
+++ commands/sendtest.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"time"
+
 	"github.com/YouDad/blockchain/api"
 	"github.com/YouDad/blockchain/log"
 	"github.com/YouDad/blockchain/network"
@@ -10,10 +12,12 @@ import (
 
 var (
 	sendTestFrom string
+	tps          int64
 )
 
 func init() {
 	SendTestCmd.Flags().StringVar(&sendTestFrom, "from", "", "Source wallet address")
+	SendTestCmd.Flags().Int64Var(&tps, "tps", 400, "send speed, transaction per second")
 	SendTestCmd.MarkFlagRequired("from")
 }
 
@@ -26,8 +30,10 @@ var SendTestCmd = &cobra.Command{
 		}
 
 		network.Register()
+		time.Sleep(time.Second)
 
 		for {
+			last := time.Now().UnixNano()
 			sendTestTo := string(wallet.NewWallet().GetAddress())
 			log.Infoln("SendTest", sendTestFrom, sendTestTo)
 			err := api.SendCMD(sendTestFrom, sendTestTo, 1)
@@ -36,6 +42,10 @@ var SendTestCmd = &cobra.Command{
 				log.Warnln("SendTest Warn?", err)
 			} else {
 				log.Infoln("SendTest Success!")
+			}
+
+			if 1e9/tps > time.Now().UnixNano()-last {
+				time.Sleep(time.Duration(1e9/tps - (time.Now().UnixNano() - last)))
 			}
 		}
 	},
