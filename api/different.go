94036ad54048ed34b31f18319c642721d962317a
manjaro-pc <1632083718@qq.com>
2020-04-19 13:43:54 +0800

    CLEAN: api/different.syncBlocks

--- api/different.go
+++ api/different.go
@@ -31,58 +31,59 @@ func syncBlocks(group int, newHeight int32, address string) {
 	lastest := bc.GetLastest()
 	originHash := lastest.Hash()
 
-	if newHeight > lastestHeight {
-		var l int32 = 0
-		var r int32 = lastestHeight
-		// log.Traceln("l", l, "r", r)
-		for r >= l {
-			mid := l + (r-l)/2
-			// log.Traceln("mid", mid)
-			block := bc.GetBlockByHeight(mid)
-			hash, err := CallbackGetHash(group, mid, address)
-			if err != nil {
-				log.Warn(err)
-				return
-			}
+	// 二分同步找到差异点
+	var l int32 = 0
+	var r int32 = lastestHeight
+	for r >= l {
+		m := l + (r-l)/2
 
-			// log.Tracef("%s === %s\n", hash, block.Hash)
-			if bytes.Compare(hash, block.Hash()) == 0 {
-				l = mid + 1
-			} else {
-				r = mid - 1
-			}
-		}
-
-		// log.Traceln("l", l, "r", r)
-		lastest := bc.GetBlockByHeight(r)
-		lastestBytes := utils.Encode(lastest)
-		bc.SetLastest(lastestBytes)
-		if lastest == nil {
-			log.Errln("二分nil")
-		}
-
-		lastestHash := lastest.Hash()
-		blocks, err := CallbackGetBlocks(group, l, newHeight, lastestHash, address)
+		block := bc.GetBlockByHeight(m)
+		hash, err := CallbackGetHash(group, m, address)
 		if err != nil {
 			log.Warn(err)
-			lastestBytes = bc.Get(originHash)
-			bc.SetLastest(lastestBytes)
 			return
 		}
 
-		set := core.GetUTXOSet(group)
-		for i := lastestHeight; i > r; i-- {
-			set.Reverse(bc.GetBlockByHeight(i))
+		if bytes.Compare(hash, block.Hash()) == 0 {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	// 将lastest移到最新的相同点
+	lastest = bc.GetBlockByHeight(r)
+	lastestBytes := utils.Encode(lastest)
+	bc.SetLastest(lastestBytes)
+	if lastest == nil {
+		// 有可能r是-1
+		log.Errln("二分nil")
+	}
+
+	// 获得group组的l到newHeight高度的区块
+	lastestHash := lastest.Hash()
+	blocks, err := CallbackGetBlocks(group, l, newHeight, lastestHash, address)
+	if err != nil {
+		log.Warn(err)
+		lastestBytes = bc.Get(originHash)
+		bc.SetLastest(lastestBytes)
+		return
+	}
+
+	// 撤销原先lastestHeight到r的高度的UTXOSet集合
+	set := core.GetUTXOSet(group)
+	for i := lastestHeight; i > r; i-- {
+		set.Reverse(bc.GetBlockByHeight(i))
+	}
+
+	// 然后将后面所有区块都追加到lastest的后面
+	for _, block := range blocks {
+		if bytes.Compare(block.PrevHash, lastestHash) != 0 {
+			break
 		}
 
-		for _, block := range blocks {
-			if bytes.Compare(block.PrevHash, lastestHash) == 0 {
-				bc.AddBlock(block)
-				set.Update(block)
-				lastestHash = block.Hash()
-			} else {
-				break
-			}
-		}
+		bc.AddBlock(block)
+		set.Update(block)
+		lastestHash = block.Hash()
 	}
 }
