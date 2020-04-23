cddc8dcb51313d072010eb8a0ddb373c3a6b4c24
manjaro-pc <1632083718@qq.com>
2020-04-23 08:40:02 +0800

    UPDATE: test/main

--- test/main.sh
+++ test/main.sh
@@ -4,17 +4,18 @@ make clean
 rm -rf *.log
 source test/define.sh $0 $1
 
-Pre="-v3 -g2 --port"
+GroupNumber=1
+Pre="-v3 -g$GroupNumber --port"
 
 declare -a Address
 
-for (( i=1; i<=2; i++ )); do
+for (( i=1; i<=GroupNumber; i++ )); do
 	RunTest create_wallet "$Pre $(( i*1111 )) --specified $(( i-1 ))" 's#.*: \(.*\)#\1#g'
 	sleep 0.1
 	Address["$(( i*1111 ))"]="${TestRegMatch}"
 done
 
-for (( i=1; i<=2; i++ )); do
+for (( i=1; i<=GroupNumber; i++ )); do
 	for (( j=1; j<=3; j++ )); do
 		RunTest create_wallet "$Pre $(( i*1100+j )) --specified $(( i-1 ))" 's#.*: \(.*\)#\1#g'
 		sleep 0.1
@@ -22,18 +23,32 @@ for (( i=1; i<=2; i++ )); do
 	done
 done
 
-for (( i=1; i<=2; i++ )); do
+for (( i=1; i<=GroupNumber; i++ )); do
 	RunTest create_blockchain "$Pre $(( i*1111 )) --address ${Address[i*1111]}"
 	sleep 0.1
 	RunTest send_test "$Pre $(( i*1111 )) --from ${Address[i*1111]}" &
 	sleep 0.1
 done
 
-for (( i=1; i<=2; i++ )); do
+function mine() {
+	i=$1
+	j=$2
+	RunTest sync "$Pre $(( i*1100+j )) --address ${Address[i*1100+j]}"
+	sleep 2
+	RunTest mining "$Pre $(( i*1100+j )) --address ${Address[i*1100+j]}" &
+	sleep 3
+}
+
+for (( i=1; i<GroupNumber; i++ )); do
 	for (( j=1; j<=3; j++ )); do
-		RunTest sync "$Pre $(( i*1100+j )) --address ${Address[i*1100+j]}"
-		sleep 0.1
-		RunTest mining "$Pre $(( i*1100+j )) --address ${Address[i*1100+j]}" &
-		sleep 0.8
+		mine $i $j
 	done
 done
+
+for (( j=1; j<=2; j++ )); do
+	mine $GroupNumber $j
+done
+
+RunTest sync "$Pre $(( GroupNumber*1100+3 )) --address ${Address[GroupNumber*1100+3]}"
+sleep 2
+RunTest mining "$Pre $(( GroupNumber*1100+3 )) --address ${Address[GroupNumber*1100+3]}"
