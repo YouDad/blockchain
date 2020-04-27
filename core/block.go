b599c8ec2f1229aba783f28ec27f8a0551e82b76
manjaro-pc <1632083718@qq.com>
2020-04-27 11:02:41 +0800

    ADD: add mutex to protect BytesToBlock

--- core/block.go
+++ core/block.go
@@ -2,10 +2,13 @@ package core
 
 import (
 	"encoding/json"
+	"sync"
 
 	"github.com/YouDad/blockchain/log"
 	"github.com/YouDad/blockchain/types"
 )
+
+var mutexToBlock sync.Mutex
 
 func BytesToBlock(bytes []byte) *types.Block {
 	if bytes == nil {
@@ -13,7 +16,9 @@ func BytesToBlock(bytes []byte) *types.Block {
 	}
 
 	block := types.Block{}
+	mutexToBlock.Lock()
 	err := json.Unmarshal(bytes, &block)
+	mutexToBlock.Unlock()
 	if err != nil {
 		log.Warn(err)
 		log.Warnf("len=%d,bytes=%x", len(bytes), bytes)
