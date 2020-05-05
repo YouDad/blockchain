4d3de87b73fe7780d4b062d39e737485a988d5c9
manjaro-pc <1632083718@qq.com>
2020-05-05 18:24:10 +0800

    CLEAN: api/base

--- api/base.go
+++ api/base.go
@@ -1,10 +1,8 @@
 package api
 
 import (
-	"encoding/json"
-	"os"
-
 	"github.com/YouDad/blockchain/log"
+
 	"github.com/astaxie/beego"
 	jsoniter "github.com/json-iterator/go"
 )
@@ -30,14 +28,6 @@ func (c *BaseController) ParseParameter(data interface{}) {
 	}
 }
 
-func (c *BaseController) GetCookie(key string) string {
-	return c.Ctx.GetCookie(key)
-}
-
-func (c *BaseController) GetToken() string {
-	return c.GetCookie("token")
-}
-
 func (c *BaseController) ReturnJson(data SimpleJSONResult) {
 	c.Data["json"] = data
 	c.ServeJSON()
@@ -56,21 +46,4 @@ func (c *BaseController) Return(data interface{}) {
 
 func (c *BaseController) Param(key string) string {
 	return c.Ctx.Input.Param(key)
-}
-
-func ReadJsonFromFile(jsonPath string, dataRef interface{}) error {
-	file, err := os.Open(jsonPath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	fileInfo, err := file.Stat()
-	log.Err(err)
-
-	data := make([]byte, fileInfo.Size())
-	_, err = file.Read(data)
-	log.Err(err)
-
-	return json.Unmarshal(data, &dataRef)
 }
